897763826c8e9353bf0d0362f486303e1d36fd89
Damon R <dmarcr1997@gmail.com>
2020-01-23 20:56:59 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,7 @@
 require 'pry'
 # Write your code here.
 
+name = ["John","Joe","Jack","Jan","Jessica"]
 def badge_maker(names)
   badge_messages = names.each do |name|
     badge_messages << "Hello, my name is #{name}."
