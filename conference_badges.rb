9e1d2f893bb931dbb6d0f99a861800dea6fa8198
Damon R <dmarcr1997@gmail.com>
2020-01-23 20:57:33 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,7 @@ require 'pry'
 name = ["John","Joe","Jack","Jan","Jessica"]
 def badge_maker(names)
   badge_messages = names.each do |name|
-    badge_messages << "Hello, my name is #{name}."
+    badge_messages.push("Hello, my name is #{name}.")
   end
   return badge_messages
 end
